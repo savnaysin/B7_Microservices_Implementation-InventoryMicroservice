51e5fe11300704b65588ed0f1d2a755926cf2d1c
Savinay Singh <savinay.singh86@gmail.com>
2019-09-23 19:37:11 -0500

    updated with PUT operation

--- src/main/java/b7/savsi/implementation/inventory/controller/InventoryController.java
+++ src/main/java/b7/savsi/implementation/inventory/controller/InventoryController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -76,6 +77,17 @@ public class InventoryController {
 		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(newInventory.getInventoryId()).toUri();
 		return ResponseEntity.created(location).build();
+	}
+
+	@PutMapping(path = "/inventory/updateInventory", consumes = "application/json", produces = "application/json")
+	public Inventory updateInventoryInfo(@RequestBody Inventory inventory) throws NotFoundException {
+		Integer productId = inventory.getProductId();
+		Inventory foundInventory = inventoryRepository.findByProductIdOrderByUpdatedOn(productId);
+		if (foundInventory == null)
+			throw new NotFoundException("No Inventory Found for the product id:: " + productId);
+		foundInventory.setQuantity(inventory.getQuantity());
+		Inventory updatedInventory = inventoryRepository.save(foundInventory);
+		return updatedInventory;
 	}
 
 }
